01048cdbef83d05e39410d87bf732a40f82bf451
Mihail Cherkezov <cherkezov.mihail@gmail.com>
2023-01-20 15:26:28 +0200

    Update rock_paper_scissors.py
    change single quotes to doubles on first if-else statement.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,11 +6,11 @@ scissors = "Scissors"
 
 player_move = input("Choose [r]ock, [p]apper or [s]cissors: ")
 
-if player_move == 'r':
+if player_move == "r":
     player_move = rock
-elif player_move == 'p':
+elif player_move == "p":
     player_move = paper
-elif player_move == 's':
+elif player_move == "s":
     player_move = scissors
 else:
     raise SystemExit("Invalid input. Try again...")
